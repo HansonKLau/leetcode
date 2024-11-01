8750efe64ae2956e3fd3bbc8bf411219aea88a96
Hanson <Hansonlau7@gmail.com>
2024-11-01 16:55:30 -0700

    interpreted validity wrong, fixed

--- Arrays and Strings/ValidParentheses.java
+++ Arrays and Strings/ValidParentheses.java
@@ -4,9 +4,11 @@ public class ValidParentheses {
     public static void main(String[] args) {
         Solution solution = new Solution();
 
-        System.out.println(solution.isValid("()"));
-        System.out.println(solution.isValid("({}[]"));
-        System.out.println(solution.isValid("[({]})"));
+        System.out.println(solution.isValid("()")); // true
+        System.out.println(solution.isValid("({}[]")); // false
+        System.out.println(solution.isValid("({[]})")); // true
+        System.out.println(solution.isValid("([)]")); // false
+        System.out.println(solution.isValid("()[]{}")); // true
 
     }
 }
@@ -14,51 +16,34 @@ public class ValidParentheses {
 class Solution {
     public boolean isValid(String s) {
 
-        int roundCount = 0;
-        int curlyCount = 0;
-        int squareCount = 0;
+        Stack<Character> charStack = new Stack<>();
+
+        List<Character> open = Arrays.asList('(', '{', '[');
+        List<Character> close = Arrays.asList(')', '}', ']');
+
+        HashMap<Character, Character> mapping = new HashMap<>();
+
+        for (int i = 0; i < open.size(); i++) {
+            mapping.put(open.get(i), close.get(i));
+        }
 
         for (int i = 0; i < s.length(); i++) {
 
-            switch (s.charAt(i)) {
-
-                case '(':
-                    roundCount++;
-                    break;
-                
-                case ')':
-                    roundCount--;
-                    break;
-                
-                case '{':
-                    curlyCount++;
-                    break;
-
-                case '}':
-                    curlyCount--;
-                    break;
-                
-                case '[':
-                    squareCount++;
-                    break;
-
-                case ']':
-                    squareCount--;
-                    break;
-
+            char currentChar = s.charAt(i);
+            if (open.contains(currentChar)) {
+                charStack.add(currentChar);
             }
 
-            // System.out.println("round: " + roundCount);
-            // System.out.println("c: " + curlyCount);
-            // System.out.println("s: " + squareCount);
-
-
-            if (roundCount < 0 || curlyCount < 0 || squareCount < 0) {
-                return false;
+            else if (close.contains(currentChar)) {
+                if (charStack.isEmpty() || currentChar != mapping.get(charStack.peek())) {
+                    return false;
+                } else {
+                    charStack.pop();
+                }
             }
+
         }
 
-        return roundCount + curlyCount + squareCount == 0;
-
+        return charStack.isEmpty();
     }
 }
